637fd129830a0858d5d32ae124e00e94471d89ad
desalabg <gregory.de-salaberry-seljak@edu.univ-grenoble-alpes.fr>
2021-05-19 21:23:01 +0200

    compute once, watch many times!

--- harmonics_vis.py
+++ harmonics_vis.py
@@ -15,14 +15,30 @@ from scipy.special import sph_harm
 coeffs = [[0],[1,1,1],[0,0,0,0,0]]
 ext_coeffs =[[0]]
 #%%
-#################################################
-#################################################
-
+##################################################
+# You don't need to rerun the program each time! #
+# Run this is in interactive console:            #
+#   $python3 -i harmonics_vis.py                 #
+#                                                #
+# Now you can manipulate and re-run the visuals  #
+# without having to recompute the fields:        #
+#                                                #
+# >>> FG.coeffs = [[0.4], [0+2j,0.1,1]]          #
+# >>> FG.see_frame()                             #
+# >>> FG.depth_movie()                           #
+##################################################
+#
 # N.B. To comply with scipy's convention, I follow this convention:
 # (theta, phi)                     # (0,0)
 #  N,S,E,W :     (pi/2, -pi) #         # (pi/2, pi)
 #                         (pi, 0) #
+#
 ################################################
+
+def main():
+    FG = FieldGenerator()
+    FG.show_field(FG.generate_frame())
+    return FG
 
 class FieldGenerator(object):
 
@@ -30,6 +46,7 @@ class FieldGenerator(object):
         self.world_img = mpimg.imread(imagepath)
         self.y_ind, self.x_ind = self.world_img.shape[0], self.world_img.shape[1]
         self.coeffs = coeffs
+        self.generate_eigenfields()
 
     def ct_sp(self, x,y):
         # Certainly the most difficult part of the project!
@@ -45,7 +62,7 @@ class FieldGenerator(object):
             phi = np.nan
         return(theta, phi)
 
-    def show_field(self, field=np.zeros((self.y_ind,self.x_ind))):
+    def show_field(self, field):
         fig = plt.imshow(self.world_img)
         plt.imshow(field, alpha=0.8, cmap='gray', vmin=np.min(field), vmax=np.max(field), interpolation="none")
         fig.axes.get_xaxis().set_visible(False)
@@ -57,30 +74,11 @@ class FieldGenerator(object):
         plt.scatter(pointx, pointy, s=5)
         plt.show()
 
-    def coeff_harm(self, theta, phi, coeffs=[[0]], radius=1):
-        value = 0
-        l_max = len(coeffs)     # ragged list of lists of coefficients
-        for l in range(l_max):
-            m_max = len(coeffs[l])
-            for m in range(m_max):
-                value += radius*(-1*l-1)*coeffs[l][m]*sph_harm(m,l,theta,phi)
-        return value
-
-    def draw_field(self):
-        flat = np.zeros((self.y_ind, self.x_ind))
-        for j in range(self.x_ind):
-            c = 0
-            for k in range (self.y_ind):
-                x = j
-                y = k - int(self.y_ind/2)
-                theta, phi = self.ct_sp(x,y)
-                if (phi > -1000):
-                    flat[k,j] = self.coeff_harm(theta, phi, coeffs, 1)
-        #self.show_field(flat)
-        return flat
-#flat = draw_field()
-
-    def indiv_harms(self, radius=1):
+    def generate_eigenfields(self):
+        """
+        Generate a ragged list of complex-valued fields over the image.
+        Computationally intensive, but it only has to be done once.
+        """
         eigenfields = [[]]
         for l in range(len(self.coeffs)):
             if (l == len(self.coeffs)-1):
@@ -90,7 +88,7 @@ class FieldGenerator(object):
             for m_i in range(len(self.coeffs[l])):
                 m = m_i - l
                 print(f" l: {l} || m: {m}")
-                field = np.zeros((self.y_ind, self.x_ind))
+                field = np.zeros((self.y_ind, self.x_ind), dtype=complex)
                 for j in range(self.x_ind):
                     if self.coeffs[l][m_i] == 0:
                         continue
@@ -98,18 +96,21 @@ class FieldGenerator(object):
                         x = j
                         y = k
                         theta, phi = self.ct_sp(x,y)
-                        field[k,j] = np.real(radius*(-1*l-1)*coeffs[l][m_i]*sph_harm(m,l, phi,theta))
+                        field[k,j] = sph_harm(m,l, phi,theta)
                 np.nan_to_num(field, copy=False)
                 eigenfields[l].append(field)
-            
-        return eigenfields
+        self.eigenfields = eigenfields
 
-    def generate_frame(self, eigenfields, radius=1):
+    def generate_frame(self, radius=1):
         net_field = np.zeros((self.y_ind, self.x_ind))
-        for l in range(len(eigenfields)):
-            for m in range(len(eigenfields[l])):
-                net_field += radius ** (-1*l-1)
+        for l in range(len(self.coeffs)):
+            for m in range(len(self.coeffs[l])):
+                net_field += np.real((radius ** (-1*l-1))*coeffs[l][m]*self.eigenfields[l][m])
         return net_field
+    
+    def see_frame(self):
+        self.show_field(self.generate_frame())
+FG = main()
 
 """
 indies1 = indiv_harms(coeffs)
